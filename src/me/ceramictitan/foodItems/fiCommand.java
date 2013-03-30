2b3cbae78d0dbfa9cbf92e41382cd07ccead57c0
blha303 <stevensmith.ome@gmail.com>
2013-03-30 18:59:28 +0800

    This is sort of what I was thinking.

--- src/me/ceramictitan/foodItems/fiCommand.java
+++ src/me/ceramictitan/foodItems/fiCommand.java
@@ -15,24 +15,44 @@ public class fiCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String CommandLabel, String[] args) {
-		if(cmd.getName().equals("fi") && args.length == 0){
-			plugin.printHelp(sender);
-			return true;	
-		}
-		if(args.length == 1){
-			sender.sendMessage(ChatColor.RED+"Not enough arugments!");
-			plugin.printHelp(sender);
-		}
-		if (args.length > 2){
-			String name = String.valueOf(args[0]);
-			if(args[1].matches("0-9")){
-				int item = Integer.valueOf(args[1]);
+		if(cmd.getName().equals("fi")) {
+			if (args.length == 0){
+				plugin.printHelp(sender);
+				return true;	
 			}
-			if(args[2].equalsIgnoreCase("confusion")){
-
+			if(args.length == 1){
+				sender.sendMessage(ChatColor.RED+"Not enough arugments!");
+				plugin.printHelp(sender);
 			}
+			// Assuming /fi add <name> <item> <effects>...
+			int x = 0;
+			int item = 0;
+			String name = "";
+			String action = "";
+			String effect = "";
+			for (String arg : args) {
+				x++;
+				if (x == 1) action = arg;
+				if (x == 2) name = arg;
+				if (x == 3) item = Integer.parseInt(arg);
+				if (x == 4) effect = arg;
+				if (x > 4) effect = effect + "," + arg;
+			}
+			String[] effects = effect.split(",");
+			for (String eff : effects) {
+				if (eff.equalsIgnoreCase("confusion")) doSomething();
+				if (eff.equalsIgnoreCase("jump")) doSomethingElse();
+			}
+			return true;
 		}
 		return false;
 	}
 
+	// http://stackoverflow.com/a/2275030
+	public boolean contains(String haystack, String needle) {
+		haystack = haystack == null ? "" : haystack;
+		needle = needle == null ? "" : needle;
+		return haystack.toLowerCase().contains(needle.toLowerCase());
+	}
+	
 }
